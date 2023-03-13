e1aab47a4719f35a0b8858b9a23070424e3a7298
Sunisc <surya.poddutoori@gmail.com>
2023-03-13 19:10:46 -0400

    First push

---
+++ src/App.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  let noteCount = 0;
+  let octave = 0;
+  
+</script>
+
+<main>
+  <div>  
+    Note range
+    <input type="text">
+  </div>
+  <div>
+    Note count
+    <input type="text">
+  </div>  
+</main>
+
+<style>
+  main {
+    display: flex;
+    flex-flow: row wrap;
+    justify-content: center;
+    gap: 1rem;
+    margin: 1rem;
+  }
+</style>
